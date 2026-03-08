c9f6b9d9043ae2fe2b2fad038f36ddfe8f8ed650
4teen Genesis <info14fourteen@gmail.com>
2026-03-08 15:00:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,29 @@ module.exports = {
 
 BOT_NAME: "4TEEN Drop Bot",
 
+// Telegram IDs
 GROUP_ID: -1003375405784,
 CHANNEL_ID: -1003233193685,
 
+// Reward settings
 MIN_REWARD: 1,
 MAX_REWARD: 5,
 
+// Token settings
 TOKEN_DECIMALS: 6,
 
-AIRDROP_CONTRACT: "TV6eXKWCsZ15c3Svz39mRQWtBsqvNNBwpQ"
+// 4TEEN token contract
+TOKEN_ADDRESS: "TMLXiCW2ZAkvjmn79ZXa4vdHX5BE3n9x4A",
 
-};
+// Airdrop vault contract
+AIRDROP_CONTRACT: "TV6eXKWCsZ15c3Svz39mRQWtBsqvNNBwpQ",
+
+// platformBit для Telegram
+TELEGRAM_PLATFORM_BIT: 4,
+
+// TRON network
+TRON_FULL_NODE: "https://api.trongrid.io",
+TRON_SOLIDITY_NODE: "https://api.trongrid.io",
+TRON_EVENT_SERVER: "https://api.trongrid.io"
+
+}
